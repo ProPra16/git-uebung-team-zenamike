1aeb74d713fc6c20bb178a58fd9dfb7e44460d31
Zeljko Bekcic <zebek101@uni-duesseldorf.de>
2016-06-17 11:21:51 +0200

    Added the 'feature' to trigger the alert

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -3,6 +3,7 @@ package de.hhu.propra16;
 import com.sun.org.apache.xml.internal.security.Init;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
@@ -21,5 +22,11 @@ public class FiveButtonsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         gridPane.add(alertButton, 0,1);
+        alertButton.setOnAction( e -> {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("YOU HAVE TOO MANI ");
+            alert.setContentText("TOO MANY BUTTONS");
+            alert.showAndWait();
+        });
     }
 }
