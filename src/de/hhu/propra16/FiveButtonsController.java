0528b57899d6aabc1a8673d984afd26355bb978a
Zeljko Bekcic <zebek101@uni-duesseldorf.de>
2016-06-17 11:12:55 +0200

    Added the button to triger an alert

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,25 @@
 package de.hhu.propra16;
 
+import com.sun.org.apache.xml.internal.security.Init;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
-public class FiveButtonsController {
+public class FiveButtonsController implements Initializable {
+
+    @FXML GridPane gridPane;
+
+    Button alertButton = new Button("ALERT!");
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        gridPane.add(alertButton, 0,1);
+    }
 }
